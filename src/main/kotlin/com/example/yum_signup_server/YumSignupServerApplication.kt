9c05aace725b59c2798d689c6821d5be1b63c047
Jun9Wo <wjddnalt24@gmail.com>
2025-02-19 14:48:00 +0900

    develop 브랜치 생성

---
+++ src/main/kotlin/com/example/yum_signup_server/YumSignupServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.yum_signup_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YumSignupServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<YumSignupServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/yum_signup_server/YumSignupServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.yum_signup_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YumSignupServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
