cdc92808a214ecf915fd31b1e4b3fde3b4809e17
Jun9Wo <wjddnalt24@gmail.com>
2025-02-20 14:38:05 +0900

    feat: 회원 정보 JPA Repository 추가

--- src/main/kotlin/com/example/yum_signup_server/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/example/yum_signup_server/member/repository/MemberRepository.kt
@@ -1,4 +1,9 @@
 package com.example.yum_signup_server.member.repository
 
-interface MemberRepository {
+import com.example.yum_signup_server.member.entity.Member
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByEmail(email: String): Optional<Member>
 }
