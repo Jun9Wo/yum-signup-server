8d749ca21e7329c3a16b1dc7a8d9e1f2fd172d42
Jun9Wo <wjddnalt24@gmail.com>
2025-02-25 13:31:15 +0900

    docs: Member 엔티티 필드 설명 주석 추가

--- src/main/kotlin/com/example/yum_signup_server/member/entity/Member.kt
+++ src/main/kotlin/com/example/yum_signup_server/member/entity/Member.kt
@@ -6,18 +6,23 @@ import jakarta.persistence.*
 @Table(name = "members")
 data class Member(
     @Id
+    // 회원 고유 ID
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
 
+    // 회원 이메일 (중복 불가)
     @Column(nullable = false, unique = true)
     val email: String,
 
+    // 회원 비밀번호
     @Column(nullable = false)
     val password: String,
 
+    // 회원의 닉네임
     @Column(nullable = false)
     val name: String,
 
+    // 회원 역할 (기본값 : USER)ß
     @Enumerated(EnumType.STRING)
     val role: MemberRole = MemberRole.USER
 )
