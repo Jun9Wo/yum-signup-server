70aedb0fe1a6641f04aa8d215109d7f67e8a9222
Jun9Wo <wjddnalt24@gmail.com>
2025-03-24 10:34:07 +0900

    docs: docs: Swagger 설정 어노테이션 기반으로 변경하여 서버 URL 설정 제거

--- src/main/kotlin/com/example/yum_signup_server/common/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/yum_signup_server/common/config/SwaggerConfig.kt
@@ -1,15 +1,21 @@
 package com.example.yum_signup_server.common.config
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition
-import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@OpenAPIDefinition(
-    info = Info(
-        title = "Yum Auth API 명세서 (회원가입 및 로그인)",
-        description = "A&I 프로젝트 Yum 인증 서버의 API 명세서입니다.",
-        version = "1.0.0"
-    )
-)
-class SwaggerConfig
+class SwaggerConfig {
+
+    @Bean
+    fun openApi(): OpenAPI = OpenAPI()
+        .components(Components())
+        .info(swaggerInfo())
+
+    private fun swaggerInfo(): Info = Info()
+        .title("Yum Auth API 명세서 (회원가입 및 로그인)")
+        .description("A&I 프로젝트 Yum 인증 서버의 API 명세서입니다.")
+        .version("1.0.0")
+}
